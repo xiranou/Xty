2bc0e39f9f30119a75b50136c30ef8603512a29d
xiranou <xiranou@gmail.com>
2015-03-12 17:14:13 -0400

    add #cart_action for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  def cart_action(current_user_id)
+    if $redis.sismember "cart#{current_user_id}", id
+      "Remove from"
+    else
+      "Add to"
+    end
+  end
 end
