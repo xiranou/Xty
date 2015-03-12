6c2b7e3e35a124bd5b19717fbc0550c79ca4843c
xiranou <xiranou@gmail.com>
2015-03-12 16:12:21 -0400

    run migration for products

--- db/migrate/20150312200556_create_products.rb
+++ db/migrate/20150312200556_create_products.rb
@@ -1,7 +1,8 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.integer :price
       t.timestamps null: false
     end
   end
