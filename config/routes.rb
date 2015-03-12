4dcf0f98db1cd6668963889364c5e0c74615744f
xiranou <xiranou@gmail.com>
2015-03-12 16:55:40 -0400

    update cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'carts/show'
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
+
   devise_for :users
+
   resources :products, only: [:index, :show]
+
   root "products#index"
 end
