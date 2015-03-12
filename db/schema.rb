d07ef25f4e0cf236c4b1094752b6819792a498b3
xiranou <xiranou@gmail.com>
2015-03-12 16:22:56 -0400

    fix products migration

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20150312200556) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.integer  "price"
+    t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
