17f9ef3a3b3504930788e4b7a5d15f8ba0e9fe33
xiranou <xiranou@gmail.com>
2015-03-16 16:04:53 -0400

    fix cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :cart, only: [:show] do
-    put 'add/:movie_id', to: 'carts#add', as: :add_to
-    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+    put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
   end
 
   devise_for :users
