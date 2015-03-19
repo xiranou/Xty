6f8730dfb122675115c1e0f18caed6ce6ff68b4b
xiranou <xiranou@gmail.com>
2015-03-19 14:29:06 -0400

    generate transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'transactions/new'
+
   resource :cart, only: [:show] do
     put 'add/:product_id', to: 'carts#add', as: :add_to
     put 'remove/:product_id', to: 'carts#remove', as: :remove_from
