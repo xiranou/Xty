77beb4df89654945b5d35be47904868e22df32b9
xiranou <xiranou@gmail.com>
2015-03-19 14:32:36 -0400

    update TransactionsController with before_actions to check user and cart

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,14 @@
 class TransactionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_cart!
   def new
+  end
+
+  private
+
+  def check_cart!
+    if current_user.get_cart_movies.blank?
+      redirect_to root_url, alert: "Your cart is empty!"
+    end
   end
 end
