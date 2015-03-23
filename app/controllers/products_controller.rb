cfed49c8e8d38e3c36b497cfc10edd7bc3f281ad
xiranou <xiranou@gmail.com>
2015-03-23 16:34:00 -0400

    update ProductsController to get current_user_id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,16 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
+  before_action :current_user_id, only: [:index, :show]
 
   def index
     @products = Product.all
   end
 
-  def show
-    @cart_action = @product.cart_action(current_user.try(:id))
-  end
-
   private
+
+  def current_user_id
+    current_user.try(:id)
+  end
 
   def set_product
     @product = Product.find(params[:id])
