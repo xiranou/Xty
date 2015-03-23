21aa81fd58bd3572e84f1122dce3109b2731e867
xiranou <xiranou@gmail.com>
2015-03-23 16:35:30 -0400

    update Product model to use redis hash

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_many :buyers, through: :purchases
 
   def cart_action(current_user_id)
-    if $redis.sismember "cart#{current_user_id}", id
+    if $redis.hexists("cart#{current_user_id}", id)
       "Remove from"
     else
       "Add to"
@@ -15,5 +15,9 @@ class Product < ActiveRecord::Base
       amount: self.price,
       payment_method_nonce: nonce
       )
+  end
+
+  def current_quantities(current_user_cart)
+    $redis.hget(current_user_cart, id)
   end
 end
