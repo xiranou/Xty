95fe2b9e1a9102dd73c4ca45b24794c8bc36033f
xiranou <xiranou@gmail.com>
2015-03-25 16:40:16 -0400

    add transaction_id column to purchases table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325175449) do
+ActiveRecord::Schema.define(version: 20150325203929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +26,10 @@ ActiveRecord::Schema.define(version: 20150325175449) do
   create_table "purchases", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "buyer_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "quantities"
+    t.string   "transaction_id"
   end
 
   create_table "users", force: :cascade do |t|
