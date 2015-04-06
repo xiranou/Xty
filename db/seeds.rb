d4ba278e594b9df1cc271e875700ab59299de012
xiranou <xiranou@gmail.com>
2015-04-06 16:26:41 -0400

    update seed file to generate states

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 def create_product(amount=10)
   amount.times do
     Product.create(
@@ -12,4 +14,8 @@ if Product.count <= 10
   create_product
 else
   create_product((10-Product.count))
+end
+
+CSV.foreach(Rails.root.join('db/seed_data', 'states.csv'), {headers: true, header_converters: :symbol}) do |row|
+  State.find_or_create_by(row.to_hash)
 end
