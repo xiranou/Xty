04adf217068c00794022d366ce17fba2e35d5d26
xiranou <xiranou@gmail.com>
2015-05-04 15:16:28 -0400

    initial #new action in AddressesController

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,4 +5,15 @@ class AddressesController < ApplicationController
     @shipping_addresses = current_user.shipping_addresses
     @billing_addresses = current_user.billing_addresses
   end
+
+  def new
+    @address = if params[:type] == "shipping"
+      current_user.shipping_addresses.new
+    elsif params[:type] == "billing"
+      current_user.billing_addresses.new
+    end
+    @city = @address.build_city
+    @zipcode = @city.zipcodes.new
+    @states = State.all
+  end
 end
