3eb52bd4f81dbac6d9291fd286b92700328cd30a
xiranou <xiranou@gmail.com>
2015-05-04 15:15:51 -0400

    add #full_address in Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@ class Address < ActiveRecord::Base
   belongs_to :city
   belongs_to :zipcode
   belongs_to :addressable, polymorphic: true
+
+  accepts_nested_attributes_for :city
+
+  def full_address
+    "#{street_address}, #{city}, #{city.state}, #{zipcode}"
+  end
 end
