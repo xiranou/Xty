f7ff56b5d101ca35b4845ae45505e132ae6f3509
xiranou <xiranou@gmail.com>
2015-05-05 15:42:47 -0400

    update addresses association for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :purchases, foreign_key: :buyer_id
   has_many :items, through: :purchases, source: :product
-  has_many :shipping_addresses, as: :addressable, class_name: "Address"
-  has_many :billing_addresses, as: :addressable, class_name: "Address"
+  has_many :shipping_addresses, as: :addressable
+  has_many :billing_addresses, as: :addressable
   has_one :artist
 
   def cart_count
