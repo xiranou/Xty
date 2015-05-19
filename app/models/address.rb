fe1c138838c10649942ec948054c6dee1199ecc4
xiranou <xiranou@gmail.com>
2015-05-19 15:37:52 -0400

    add before_create action to format string being saved for street_address and city name

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,17 @@
 class Address < ActiveRecord::Base
+  before_create :capitalize_name
+
   belongs_to :city
   belongs_to :zipcode
   belongs_to :addressable, polymorphic: true
 
-  accepts_nested_attributes_for :city
-
   def full_address
-    "#{street_address}, #{city.name}, #{city.state.abbrv}, #{zipcode}"
+    "#{street_address}, #{city.name}, #{city.state.abbrv}, #{zipcode.zip}"
+  end
+
+  private
+
+  def capitalize_name
+    self.street_address = self.street_address.split(" ").map(&:capitalize).join(" ")
   end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,13 @@
 class City < ActiveRecord::Base
+  before_create :capitalize_name
+
   has_many :addresses
   has_many :zipcodes
   belongs_to :state
 
-  accepts_nested_attributes_for :zipcodes
+  private
+
+  def capitalize_name
+    self.name = self.name.split(" ").map(&:capitalize).join(" ")
+  end
 end
