7d8bdb158303f146d8598adfabb57eaa28b17209
xiranou <xiranou@gmail.com>
2015-05-19 15:44:54 -0400

    add AddressesController with create addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,13 +7,58 @@ class AddressesController < ApplicationController
   end
 
   def new
-    @address = if params[:type] == "shipping"
-      current_user.shipping_addresses.new
-    elsif params[:type] == "billing"
-      current_user.billing_addresses.new
+    @address = new_address
+    @city, @zipcode, @states = City.new, Zipcode.new, State.all
+  end
+
+  def create
+    city = City.find_or_create_by(city_params)
+    zipcode = Zipcode.find_or_create_by(zipcode_params.merge({city: city}))
+
+    address = new_address(city, zipcode)
+
+    if address.save!
+      redirect_to addresses_path, notice: "Save!"
+    else
+      redirect_to new_address_path(params[:tyepe]), alter: address.errors.full_messages
     end
-    @city = @address.build_city
-    @zipcode = @city.zipcodes.new
-    @states = State.all
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit!
+  end
+
+  def city_params
+    address_params[:city]
+  end
+
+  def zipcode_params
+    address_params[:zipcode]
+  end
+
+  def new_address(city=nil, zipcode=nil)
+    if city.nil? && zipcode.nil?
+      build_address
+    else
+      create_address(city, zipcode)
+    end
+  end
+
+  def build_address
+    if params[:type] == "shipping"
+      current_user.shipping_addresses.build
+    else
+      current_user.billing_addresses.build
+    end
+  end
+
+  def create_address(city, zipcode)
+    if params[:type] == 'shipping'
+      current_user.shipping_addresses.new(address_params.merge({city: city}).merge({zipcode: zipcode}))
+    else
+      current_user.billing_addresses.new(address_params.merge({city: city}).merge({zipcode: zipcode}))
+    end
   end
 end
