298e682a13d563fcbcdab1961566ee8323f53b5b
xiranou <xiranou@gmail.com>
2015-05-20 16:06:44 -0400

    initial billing_address for Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,8 @@ class Artist < ActiveRecord::Base
   belongs_to :user
   has_one :merchant_address, as: :addressable, class_name: "Address"
   has_many :products
+
+  def billing_addresses
+    user.billing_addresses
+  end
 end
