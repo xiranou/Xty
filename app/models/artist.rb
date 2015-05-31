5dab491c4408f51ae3546c13968541611c27ba2a
xiranou <xiranou@gmail.com>
2015-05-31 19:33:47 -0400

    add tos validation in Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ActiveRecord::Base
   belongs_to :user
   has_one :merchant_address, as: :addressable, class_name: "Address"
   has_many :products
+  validates :tos, acceptance: true
 
   def billing_addresses
     user.billing_addresses
