86a67a5a016f641ebb316de7cb2a346001fee4df
xiranou <xiranou@gmail.com>
2015-06-03 17:57:42 -0400

    refactor artist merchant status update

--- app/controllers/braintreehooks_controller.rb
+++ app/controllers/braintreehooks_controller.rb
@@ -13,8 +13,12 @@ class BraintreehooksController < ApplicationController
       params[:bt_payload]
     )
     if notification.kind == Braintree::WebhookNotification::Kind::SubMerchantAccountApproved
-      artist = Artist.find_by(braintree_id: notification.merchant_account.id)
-      artist.update_attributes(merchant_status: notification.merchant_account.status)
+      Artist.where(
+        braintree_id: notification.merchant_account.id
+        )
+        .update_all(
+          merchant_status: notification.merchant_account.status
+        )
       render nothing: true, status: 200
     else
       redirect_to new_artists_path, alert: notification.message
